9cd4a7031754642bcf0475543a66ae8041ca63a1
Anjali Saini <anjalisaini8996@gmail.com>
2026-07-28 21:27:08 +0530

    Update helpers.js

---
+++ src/utils/helpers.js
@@ -0,0 +1,24 @@
+export const formatTime = (seconds) => {
+  if (!seconds) return "0:00";
+
+  const mins = Math.floor(seconds / 60);
+  const secs = Math.floor(seconds % 60);
+
+  return `${mins}:${secs.toString().padStart(2, "0")}`;
+};
+
+export const shuffleArray = (array) => {
+  const arr = [...array];
+
+  for (let i = arr.length - 1; i > 0; i--) {
+
+    const j = Math.floor(Math.random() * (i + 1));
+
+    [arr[i], arr[j]] = [arr[j], arr[i]];
+  }
+
+  return arr;
+};
+
+export const randomItem = (array) =>
+  array[Math.floor(Math.random() * array.length)];
